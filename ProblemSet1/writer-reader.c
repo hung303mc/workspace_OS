899b0630bf42c5a8e9f63f9e2051f518a3b5f4f6
hungnm27 <hungnm27@fsoft.com.vn>
2017-03-28 00:34:03 +0700

    Update write-reader code init version

--- ProblemSet1/writer-reader.c
+++ ProblemSet1/writer-reader.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
+#define NUM_THREAD 10
+#define NUM_EXE 10
 
 int sharedVar = 0;  /* shared variable */
-int add = 0;        /* place to add next element */
-int rem = 0;        /* place to remove next element */
-int num = 0;        /* number elements in buffer */
+int numReader = 0;  /* Number of reader thread */
 
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;    /* mutex lock for buffer */
-pthread_cond_t c_cons = PTHREAD_COND_INITIALIZER; /* reader waits on this cond var */
-pthread_cond_t c_prod = PTHREAD_COND_INITIALIZER; /* writer waits on this cond var */
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;    /* mutex lock for shared var */
+pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER;    /* mutex lock for number of reader*/
+pthread_cond_t c_write = PTHREAD_COND_INITIALIZER; /* writer waits on this cond var */
 
 void *writer (void *param);
 void *reader (void *param);
@@ -25,77 +26,80 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Unable to create writer thread\n");
     exit(1);
   }
-
   if(pthread_create(&tid2, NULL, reader, NULL) != 0) {
     fprintf(stderr, "Unable to create reader thread\n");
     exit(1);
   }
-
   /* wait for created thread to exit */
-  pthread_join(tid1, NULL);
-  pthread_join(tid2, NULL);
+  for (i = 0; i < NUM_THREAD; ++i)
+  {
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);    
+  }
   printf("Parent quiting\n");
-
   return 0;
 }
 
-/* Produce value(s) */
+/* Write value(s) */
 void *writer(void *param) {
+  int cntWri;
+  for (cntWri = 0; cntWri < NUM_EXE; ++cntWri)
+  {
+    /* wait rands*/
+    usleep((rand()%50)*1000*5);
 
-  int i;
-  for (i=1; i<=20; i++) {
-    
-    /* Insert into buffer */
-    pthread_mutex_lock (&m);  
-      if (num > BUF_SIZE) {
-        exit(1);  /* overflow */
-      }
+    /* Write into shared Var */
+    pthread_mutex_lock (&m1);  
 
-      while (num == BUF_SIZE) {  /* block if buffer is full */
-        pthread_cond_wait (&c_prod, &m);
-      }
-      
-      /* if executing here, buffer not full so add element */
-      buffer[add] = i;
-      add = (add+1) % BUF_SIZE;
-      num++;
-    pthread_mutex_unlock (&m);
+    /*Block if there is any reader thread*/
+    while(numReader != 0)
+    {
+      printf("numReader > 0, wait to write ...\n");
+      pthread_cond_wait(&c_write,&m1);
+    }
+    sharedVar = cntWri;
+    /* if executing here, there is no read thread */
+    pthread_mutex_unlock (&m1);
 
-    pthread_cond_signal (&c_cons);
-    printf ("writer: inserted %d\n", i);
-    fflush (stdout);
+    printf("write value %d \n",cntWri);
+    printf("number of reader is %d \n",numReader); /*It should be = 0*/
   }
-
-  printf("writer quiting\n");
-  fflush(stdout);
   return 0;
 }
 
-/* Consume value(s); Note the reader never terminates */
+/* Read value(s); Note the reader never terminates */
 void *reader(void *param) {
+  int cntRead;
+  int readVal;
 
-  int i;
+    pthread_mutex_lock(&m2);
+    /*increase numReader*/    
+      numReader++;
+    pthread_mutex_unlock(&m2);
+  for (int cntRead = 0; cntRead < NUM_EXE; ++cntRead)
+  {
+    /* wait rands*/
+    usleep((rand()%50)*1000*20);
 
-  while(1) {
-
-    pthread_mutex_lock (&m);
-      if (num < 0) {
-        exit(1);
-      } /* underflow */
-
-      while (num == 0) {  /* block if buffer empty */
-        pthread_cond_wait (&c_cons, &m);
-      }
-
-      /* if executing here, buffer not empty so remove element */
-      i = buffer[rem];
-      rem = (rem+1) % BUF_SIZE;
-      num--;
-    pthread_mutex_unlock (&m);
-
-    pthread_cond_signal (&c_prod);
-    printf ("Consume value %d\n", i);  fflush(stdout);
-
+  
+    /* Read from shared Var update numReader*/
+    pthread_mutex_lock (&m1);
+    /* if executing here, buffer not empty so remove element */
+      readVal = sharedVar;        
+    pthread_mutex_unlock (&m1);
+  
+    printf("read value %d \n",readVal);
+    printf("number of reader is %d \n",numReader);  
   }
+    pthread_mutex_lock(&m2);
+    /*increase numReader*/    
+      numReader--;
+    pthread_mutex_unlock(&m2);
+
+    /* If no reader --> generate signal to writer*/
+    if(numReader == 0){
+      pthread_cond_broadcast(&c_write);
+    }
+
   return 0;
 }
